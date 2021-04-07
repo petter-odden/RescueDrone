0bf4e6056b19ac160b7611b50e717fc703fbda3d
petter-odden <petter.odden95@gmail.com>
2021-04-08 00:43:13 +0200

    Basic login, hint-pynting

--- app/src/main/java/View/Login.kt
+++ app/src/main/java/View/Login.kt
@@ -2,11 +2,41 @@ package View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.rescuedrone.R
+import com.example.rescuedrone.databinding.ActivityLoginBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val passwordField: TextInputEditText = findViewById(R.id.password_field)
+        val emailField : TextInputEditText = findViewById(R.id.email_field)
+
+
+        emailField.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus)
+                emailField.hint = ""
+            else
+                emailField.hint = "Email Address"
+        }
+
+        passwordField.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus)
+                passwordField.hint = ""
+            else
+                emailField.hint = "Password"
+        }
+
+
     }
+
+
 }
