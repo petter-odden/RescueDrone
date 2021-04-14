a30fd97aad06793775604460d1ad016dd72c88fa
SSoppen <SSoppen@users.noreply.github.com>
2021-04-14 14:02:27 +0200

    Created constructor for EmergencyPersonnel

--- app/src/main/java/Model/EmergencyPersonnel.java
+++ app/src/main/java/Model/EmergencyPersonnel.java
@@ -2,4 +2,7 @@ package Model;
 
 public class EmergencyPersonnel extends Person {
 
+    public EmergencyPersonnel(String name, String UID, String username) {
+        super(name, UID, username);
+    }
 }
