258bc587af2b6b9b5119453e42ce431d9e28ef52
SSoppen <SSoppen@users.noreply.github.com>
2021-04-14 13:18:10 +0200

    Created person and pilot class

---
+++ app/src/main/java/Model/Person.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Person {
+    private String name;
+    private String UID;
+    private String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ app/src/main/java/Model/Pilot.java
@@ -0,0 +1,5 @@
+package Model;
+
+public class Pilot extends Person {
+    private boolean confirmed;
+}
